48be8029ff8dfcb3c0cf542b0a10446a201bbaa8
Koopa <codingkoopa@gmail.com>
2023-03-07 07:07:19 -0500

    driveutils: unit

---
+++ stage1/DriveUtils.js
@@ -0,0 +1,30 @@
+// Retrieves a subfolder by name.
+function getFolderByName_(parentFolder, name) {
+  const folders = parentFolder.getFoldersByName(name);
+  if (!folders.hasNext()) { 
+    throw new EntityNotFound(`Folder "${parentFolder.getName()}/${name}"" not found.`);
+  }
+  const folder = folders.next();
+  if (folders.hasNext()) {
+    throw new Error(`More than one folder "${parentFolder.getName()}/${name}" found.`);
+  }
+  return folder;
+}
+
+// Retrieves a folder by starting from the parent folder, and
+// descending into each component of the path array.
+function getFolderByPath_(parentFolder, path) {
+  let childFolder = parentFolder;
+  for (const component of path) {
+    try {
+      childFolder = getFolderByName_(childFolder, component);
+    } catch (e) {
+      if (e instanceof EntityNotFound) {
+        return null;
+      } else {
+        throw e;
+      }
+    }
+  }
+  return childFolder;
+}
